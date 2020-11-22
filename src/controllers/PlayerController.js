ab8448e9acbf009bd594235f62ec245c80fe538d
Thiago Ferreira <thiferr09@gmail.com>
2020-11-22 19:22:28 -0300

    Projeto pronto para deploy

--- src/controllers/PlayerController.js
+++ src/controllers/PlayerController.js
@@ -16,17 +16,74 @@ module.exports = {
     },
 
     async index(req, res) {
-        const player = await Player.find();
+        try{
+            const player = await Player.find();
 
-        return res.json(player);
+            return res.json(player);
+        } catch(err) {
+            console.error(err);
+        }
     },
 
     async show(req, res) {
-        const { id } = req.params;
+        try{
+            const { id } = req.params;
 
-        const player = Player.findById({_id: id});
+            const player = await Player.findById(id);
 
-        return res.json(player);
+            return res.json(player);
+        } catch(err) {
+            console.error(err);
+        }
+    },
+
+    async update(req, res) {
+        try{
+            const { wins, loses, goalsScored, concededGoals } = req.body;
+            const { id } = req.params;
+            
+            const score = parseInt(wins) * 3;
+            const goalDifference = parseInt(goalsScored) - parseInt(concededGoals);
+            const gamesPlayed = parseInt(wins) + parseInt(loses);
+
+            const newPlayer = {
+                score,
+                wins,
+                loses,
+                goalsScored,
+                concededGoals,
+                goalDifference,
+                gamesPlayed
+            }
+
+            const updatedPlayer = await Player.findOneAndUpdate({ _id: id }, newPlayer);
+            
+            return res.json(updatedPlayer);
+
+        } catch(err) {
+            console.error(err);
+        }
+    },
+
+    async deleteOne(req, res) {
+        try{
+            const { id } = req.params;
+
+            const deletedPlayer = await Player.findByIdAndDelete({_id: id});
+
+            return res.json(deletedPlayer);
+        } catch(err) {
+            console.error(err)
+        }
+    },
+
+    async deleteAll(req, res) {
+        try{
+            const deletedPlayers = await Player.deleteMany()
+
+            return res.json(deletedPlayers);
+        } catch(err) {
+            console.error(err);
+        }
     }
-
 }
--- src/routes.js
+++ src/routes.js
@@ -4,8 +4,14 @@ const Routes = express.Router();
 
 const PlayerController = require('./controllers/PlayerController');
 
-Routes.post('/users', PlayerController.create);
 Routes.get('/users', PlayerController.index);
 Routes.get('/users/:id', PlayerController.show);
+
+Routes.post('/users', PlayerController.create);
+
+Routes.put('/users/:id', PlayerController.update);
+
+Routes.delete('/users/:id', PlayerController.deleteOne);
+Routes.delete('/users', PlayerController.deleteAll);
 
 module.exports = Routes;
