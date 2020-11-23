734453d96097ba2d39205794cd1f287b2e0a0000
Thiago Ferreira <thiferr09@gmail.com>
2020-11-23 11:31:28 -0300

    Resetando todos os players

--- src/controllers/PlayerController.js
+++ src/controllers/PlayerController.js
@@ -77,7 +77,7 @@ module.exports = {
                 gamesPlayed: 0
             }
 
-            const reset = await Player.updateMany({}, resetPlayers);
+            const reset = await Player.update({ name: { $ne: "" } }, resetPlayers);
 
             return res.json(reset);
         } catch(err) {
