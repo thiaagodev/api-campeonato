4d28a40ae644668ad657d68202c360b36c6ee444
Thiago Ferreira <thiferr09@gmail.com>
2020-11-24 13:11:06 -0300

    Criando rotas e funcoes do controller do historico de partidas

---
+++ src/controllers/MatchHistoryController.js
@@ -0,0 +1,30 @@
+const MatchHistory = require('../models/MatchHistory');
+
+module.exports = {
+    async index(req, res) {
+        const history = await MatchHistory.find();
+
+        return res.json(history);
+    },
+
+    async create(req, res) {
+        const { winnerName, winnerGoals, loserName, loserGoals } = req.body;
+
+        const match = await MatchHistory.create({
+            winnerName, 
+            winnerGoals, 
+            loserName, 
+            loserGoals
+        });
+
+        res.json(match);
+    },
+
+    async delete(req, res) {
+        const { id } = req.params;
+
+        const deletedMatch = await MatchHistory.findByIdAndDelete({ _id: id });
+
+        res.json(deletedMatch);
+    }
+}
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,9 @@ const express = require('express');
 const Routes = express.Router();
 
 const PlayerController = require('./controllers/PlayerController');
+const MatchHistoryController = require('./controllers/MatchHistoryController');
+
+// Players Routes
 
 Routes.get('/players', PlayerController.index);
 Routes.get('/players/:id', PlayerController.show);
@@ -14,5 +17,13 @@ Routes.put('/reset-players', PlayerController.resetAll);
 
 Routes.delete('/players/:id', PlayerController.deleteOne);
 Routes.delete('/players', PlayerController.deleteAll);
+
+// Match History Routes
+
+Routes.get('/match-history', MatchHistoryController.index);
+
+Routes.post('/match-history', MatchHistoryController.create);
+
+Routes.delete('/match-history/:id', MatchHistoryController.delete);
 
 module.exports = Routes;
