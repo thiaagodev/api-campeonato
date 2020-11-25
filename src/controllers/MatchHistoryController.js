1ecc28eba6b61706ff6cbf31ba66957997068e9c
Thiago Ferreira <thiferr09@gmail.com>
2020-11-25 17:10:01 -0300

    Adicionando funcionalidade deleteAll

--- src/controllers/MatchHistoryController.js
+++ src/controllers/MatchHistoryController.js
@@ -1,30 +1,53 @@
 const MatchHistory = require('../models/MatchHistory');
+const { deleteAll } = require('./PlayerController');
 
 module.exports = {
     async index(req, res) {
-        const history = await MatchHistory.find().sort({ createdAt: 'desc' });
+        try {
+            const history = await MatchHistory.find().sort({ createdAt: 'desc' });
 
-        return res.json(history);
+            return res.json(history);
+        } catch(err) {
+            console.error(err)
+        }
     },
 
     async create(req, res) {
-        const { winnerName, winnerGoals, loserName, loserGoals } = req.body;
+        try {
+            const { winnerName, winnerGoals, loserName, loserGoals } = req.body;
 
-        const match = await MatchHistory.create({
-            winnerName, 
-            winnerGoals, 
-            loserName, 
-            loserGoals
-        });
-
-        res.json(match);
+            const match = await MatchHistory.create({
+                winnerName, 
+                winnerGoals, 
+                loserName, 
+                loserGoals
+            });
+    
+            res.json(match);
+        } catch(err) {
+            console.error(err)
+        }
     },
 
     async delete(req, res) {
-        const { id } = req.params;
+        try {
+            const { id } = req.params;
 
-        const deletedMatch = await MatchHistory.findByIdAndDelete({ _id: id });
+            const deletedMatch = await MatchHistory.findByIdAndDelete({ _id: id });
+    
+            res.json(deletedMatch);
+        } catch(err) {
+            console.error(err)
+        }
+    },
 
-        res.json(deletedMatch);
+    async deleteAll(req, res) {
+        try {
+            const deletedMatches = await MatchHistory.deleteMany();
+        
+            res.json(deletedMatches);
+        } catch(err) {
+            console.error(err)
+        }
     }
 }
